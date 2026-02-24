d397f0aaf2cbd313660b59d7b29b787755b38c49
karthikvch <karthik998.ch@gmail.com>
2026-02-24 22:30:37 +0530

    terraform first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+    description = "The AWS region to deploy resources in."
+    type        = string
+}
+
+variable "bucket_name" {
+    description = "The name of the S3 bucket to create."
+    type        = string
+}
+
+
